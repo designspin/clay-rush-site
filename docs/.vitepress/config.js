6fd5b8bba8d56effff5b3a08246740477da0ebbc
Jason Foster <jason@designspin.co.uk>
2026-04-03 21:40:51 +0100

    Working light/dark mode toggle with Clay Rush branding in both modes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,6 @@ export default defineConfig({
   title: 'Clay Rush',
   description: 'A fast-paced skeet shooting game for iOS',
   base: '/clay-rush-site/',
-  appearance: 'dark',
   head: [
     ['link', { rel: 'icon', href: '/clay-rush-site/app-icon.png' }]
   ],
